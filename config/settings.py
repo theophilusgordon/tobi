bc75e79a95a8703aff8ed9d41dd22d94476b972b
Theophilus Gordon <gordonfiifi@gmail.com>
2025-02-14 12:23:18 +0000

    feat: add Django REST framework and CORS headers support

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_cors_headers',
+    'djangorestframework_simplejwt',
+
 ]
 
 MIDDLEWARE = [
